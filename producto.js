bc05bedb838125366a119c802e610cc89684b7ef
HernanBueno <40526403+HernanBueno@users.noreply.github.com>
2022-02-17 13:48:27 -0300

    Delete producto.js

--- producto.js
+++
@@ -1,9 +0,0 @@
-class Producto {
-    constructor(titulo, precio, thumbnail) {
-        this.titulo = titulo;
-        this.precio = precio;
-        this.thumbnail = thumbnail;
-    }
-}
-
-module.exports = { Producto };
