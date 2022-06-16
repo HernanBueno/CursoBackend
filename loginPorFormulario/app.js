f295161549aab445874e6c150ae6e900a2b9eead
Hernan Bueno <hbueno@teco.com.ar>
2022-06-16 14:14:57 -0300

    agrego ejercicios mocksyNormalizacion y login

---
+++ loginPorFormulario/app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cookie_parser = require("cookie-parser");
+const session = require("express-session");
+const app = express();
+
+app.use(cookie_parser());
+app.use(
+  session({
+    secret: "vamolaescaloneta",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(express.json())
+
+let arr = []
+app.use((req,res,next)=>{
+    
+    if (arr)
+
+    return next();
+})
+
+
+
+let count=0;
+app.get('/mem-count',(req,res)=>{
+    res.send({count:count++})
+
+
+})
+
+app.post('/login',(req,res)=>{
+    
+    const {user,password} = req.body;
+
+})
+
+
+app.get("/", (req, res) => {
+  res.send("Express-session server");
+});
+app.get("/my-session", (req, res) => {
+  const { nombre } = req.query;
+  if (req.session.count) {
+    req.session.count++;
+    res.send(`${nombre} el números de visitas es ${req.session.count}`);
+  } else {
+    req.session.count = 1;
+    res.send(`Welcome to my server ${nombre}`);
+  }
+});
+app.get("/destroy", (req, res) => {
+  req.session.destroy((err) => {
+    if (!err) res.send("Ok");
+    else res.send({ error: err });
+  });
+});
+app.get('/info',(req,res)=>{
+    console.log(req.session)
+    res.json(req.session)
+})
+
+app.listen(6060, () => {
+  console.log("Ready session server...");
+});
